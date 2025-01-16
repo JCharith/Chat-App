64e3e75f6ad790567c7c738734990e4633e2fa81
sainaidu401 <sainaidupotnuru1@gmail.com>
2025-01-16 15:32:18 +0530

    io

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,26 +11,30 @@ const io = new Server(server, {
   },
 });
 
+const userSocketMap = {};
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (!userId) {
+    socket.disconnect();
+    return;
+  }
 
-  // io.emit() is used to send events to all the connected clients
+  userSocketMap[userId] = socket.id;
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    if (userId) {
+      delete userSocketMap[userId];
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    }
   });
 });
 
