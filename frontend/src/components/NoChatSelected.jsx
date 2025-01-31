d1918a9cecf8f49cf950a03e74988ce4766bb5ad
Charith J <charith.gnt@gmail.comclear>
2025-01-31 10:23:06 +0530

    Adding new changes

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -1,19 +1,14 @@
-import { MessageSquare } from "lucide-react";
-
 const NoChatSelected = () => {
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
-        {/* Icon Display */}
-        <div className="flex justify-center gap-4 mb-4">
-          <div className="relative">
-            <div
-              className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
-             justify-center animate-bounce"
-            >
-              <MessageSquare className="w-8 h-8 text-primary " />
-            </div>
-          </div>
+        {/* Image Display */}
+        <div className="flex justify-center mb-4">
+          <img
+            src="/favicon.jpg"
+            alt="Chat Icon"
+            className="w-16 h-16 rounded-lg object-cover"
+          />
         </div>
 
         {/* Welcome Text */}
