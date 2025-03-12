a4deabd948f120f3a3c0590ef90fb72ef0a4228d
Charith J <charith.gnt@gmail.comclear>
2025-03-12 13:23:51 +0530

    Changes done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,8 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000
     });
 
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
